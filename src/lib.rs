cab15547f199e154bcf2f5ca11262128f38606fe
RustyNixieTube <RustyNixieTube@users.noreply.github.com>
2020-08-11 00:42:52 +0200

    add macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[macro_export]
+macro_rules! debug_print {
+    ($($arg:tt)*) => (#[cfg(debug_assertions)] print!($($arg)*));
+}
+
+#[macro_export]
+macro_rules! debug_println {
+    ($($arg:tt)*) => (#[cfg(debug_assertions)] println!($($arg)*));
+}
+#[macro_export]
+macro_rules! debug_eprint {
+    ($($arg:tt)*) => (#[cfg(debug_assertions)] eprint!($($arg)*));
+}
+
+#[macro_export]
+macro_rules! debug_eprintln {
+    ($($arg:tt)*) => (#[cfg(debug_assertions)] eprintln!($($arg)*));
 }
